0f6a692e2c577ffb0aa0ff7706675fa3a4c37bbf
chenyaqi <terrence.yaqi.chen@u.nus.edu>
2026-06-22 15:38:28 +0800

    Initial commit

---
+++ src/test/java/com/foodmind/FoodmindBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodmind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodmindBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
